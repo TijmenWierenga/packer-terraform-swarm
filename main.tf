4a3b6f73e2648f31c519f1f3aa5473fbb19cca00
Tijmen Wierenga <twieren0@xs4all.net>
2018-07-22 20:59:04 +0200

    Updated cluster with registration of extra managers

--- main.tf
+++ main.tf
@@ -16,6 +16,49 @@ resource "aws_instance" "master" {
   vpc_security_group_ids = [
     "${aws_security_group.docker.id}"
   ]
+
+  connection {
+     user = "ubuntu"
+     private_key = "${file("keys/cluster")}"
+   }
+
+  provisioner "remote-exec" {
+    inline = [
+      "docker swarm init --advertise-addr ${self.private_ip}"
+    ]
+  }
+
+  provisioner "local-exec" {
+    command = "ssh -o 'StrictHostKeyChecking no' -i 'keys/cluster' ubuntu@${self.public_ip} 'docker swarm join-token manager -q' >> artifacts/manager-token.txt"
+  }
+}
+
+resource "aws_instance" "manager" {
+  ami           = "ami-1de3e562"
+  instance_type = "t2.micro"
+  key_name      = "${aws_key_pair.access_key.key_name}",
+  vpc_security_group_ids = [
+    "${aws_security_group.docker.id}"
+  ]
+  count         = 2
+  depends_on = ["aws_instance.master"]
+
+  connection {
+    user = "ubuntu"
+    private_key = "${file("keys/cluster")}"
+  }
+
+  provisioner "file" {
+    source = "artifacts/manager-token.txt"
+    destination = "/var/tmp/manager-token.txt"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "docker swarm join --token $(cat /var/tmp/manager-token.txt) --advertise-addr ${self.private_ip}:2377",
+      "rm /var/tmp/manager-token.txt"
+    ]
+  }
 }
 
 resource "aws_security_group" "docker" {
@@ -76,6 +119,10 @@ resource "aws_security_group" "docker" {
   }
 }
 
-output "ip" {
-  value = "${aws_instance.master.*.public_ip}"
+output "master_ip" {
+  value = "${aws_instance.master.public_ip}"
+}
+
+output "manager_ips" {
+  value = "${aws_instance.manager.*.public_ip}"
 }
